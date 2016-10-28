3827b9bb41397e72f89b64e9eef429665d167cf3
Enrique Silva <enrique.r.silva@gmail.com>
2016-10-27 22:57:22 -0700

    I wrote some Python

--- students/enrique_silva/session03/mailroom.py
+++ students/enrique_silva/session03/mailroom.py
@@ -38,6 +38,7 @@ def send_thanks(donor_list):
     """Send a thank you note to donor, add the donor to the list, or  accept
     a new donation"""
     user_name=str(input("""Type "list" to see donors in database or  enter first name and last name: """))
+    #TODO need to add validation on input from the user. need both to validate entries other than name or list and also account for capitalization of names and whitespace/
     print()
     if user_name== 'list':
         print_donor_list(donor_list)
@@ -55,8 +56,8 @@ def search_donor_list(donor_list, user_name):
     for ind, donor in enumerate(donor_list):
         if user_name in donor:
         	existing_donor_index=ind
-        	print(existing_donor_index)
-        	print(donor_list)
+        	#print(existing_donor_index)--check to make sure my program is doing the right thing, REMOVE
+        	#print(donor_list)
         	prompt_donation_existing(donor_list, user_name,existing_donor_index)
         	break
     else:
@@ -68,21 +69,38 @@ def search_donor_list(donor_list, user_name):
 def prompt_donation_existing(donor_list, user_name,existing_donor_index):
      """ensure donation amount is an int and add donation to list for the existing donor"""
      donation_amount= int(input('How much would you like to donate? '))
-     donor_list[existing_donor_index][1].append(donation_amount)
+     #TO DO add exception handling on invalid input
      print()
+     donor_list[existing_donor_index][1].append(donation_amount)
+     send_email(donor_list, user_name,donation_amount)
 
 def prompt_donation_new(donor_list, user_name):
      """ensure donation amount is an int and add donation to list for the new donor"""
      donation_amount= int(input('How much would you like to donate? '))
+     #TO DO add exception handling on invalid input
      print()
      donor_list[-1].append([donation_amount])
+     send_email(donor_list, user_name,donation_amount)
 
-
-
-
+def send_email(donor_list, user_name, donation_amount):
+    """write and send email to donor, thanking them for donation"""
+    print('Hello {},\n\nThank you for generous danation of {:d}.\n\nSincerely, THE STAFF'.format(user_name,donation_amount))
 
 
 def print_report(donor_list):
-    print("This will print a report")
+    """print a report, that lists donor, total given, number of gifts, and average donation amounts"""
+    report_entries=[]
+    for (donor,donations) in donor_list:
+        total_donations = sum(donations)
+        count_gifts = len(donations)
+        avg_donation = total_donations / count_gifts
+        report_entries.append((donor, total_donations, count_gifts,avg_donation))
+
+    print("{:25s} | {:11s} | {:9s} | {:12s}".format(
+          "Donor Name", "Total Donated", "Num Gifts", "Average Gift"))
+    print("-" * 66)
+    for row in report_entries:
+        print("{:25s}   {:11.2f}   {:9d}   {:12.2f}".format(*row))
 
 main(donor_list)
+
